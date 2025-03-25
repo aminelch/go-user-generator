59350a8d3fdd3a927c9243789776fa4e829bc3ad
Amine LOUHICHI <aminelch@pm.me>
2025-03-25 02:02:13 +0100

    Improve healthHandler, update user infos in listing

--- handlers.go
+++ handlers.go
@@ -1,33 +1,56 @@
 package main
 
 import (
-    "github.com/gin-gonic/gin"
-    "math/rand"
-    "net/http"
+	"github.com/gin-gonic/gin"
+	"math/rand"
+	"net/http"
+	"os"
+	"time"
 )
 
+var (
+	startTime  = time.Now()
+	appVersion = getEnv("APP_VERSION", "1.0.0")
+)
+
+func getEnv(key, defaultValue string) string {
+	if value, exists := os.LookupEnv(key); exists {
+		return value
+	}
+	return defaultValue
+}
+
+func getHostname() string {
+	if name, err := os.Hostname(); err == nil {
+		return name
+	}
+	return getEnv("HOSTNAME", "unknown-server")
+}
+
 func HealthHandler(c *gin.Context) {
-    Logger.Info("Received request on /health")
-    c.JSON(http.StatusOK, gin.H{
-        "status":  "OK",
-        "version": "1.0.0",
-        "maintainer": "Amine LOUHICHI",
-    })
+	Logger.Info("Received request on /health")
+	c.JSON(http.StatusOK, gin.H{
+		"hostname":  getHostname(),
+		"uptime":    startTime.UTC().Format(time.RFC3339),
+		"status":    "OK",
+		"timestamp": time.Now().UTC().Format(time.RFC3339),
+		"version":   appVersion, // Version dynamique
+	})
 }
 
 func GenerateHandler(c *gin.Context) {
-    Logger.Info("Received request on /generate")
+	Logger.Info("Received request on /generate")
 
-    var users []User
-    result := DB.Find(&users)
-    if result.Error != nil || len(users) == 0 {
-        Logger.Warn("No users found in the database")
-        c.JSON(http.StatusNotFound, gin.H{"error": "No user found"})
-        return
-    }
+	var users []User
+	result := DB.Find(&users)
+	if result.Error != nil || len(users) == 0 {
+		Logger.Warn("No users found in the database")
+		c.JSON(http.StatusNotFound, gin.H{"error": "No user found"})
+		return
+	}
 
-    randomUser := users[rand.Intn(len(users))]
+	randomUser := users[rand.Intn(len(users))]
 
-    Logger.Infof("Selected user: %s (%s)", randomUser.Name, randomUser.Email)
-    c.JSON(http.StatusOK, randomUser)
+	Logger.Infof("Selected user: %s %s (%s)", randomUser.Uuid, randomUser.Name, randomUser.Email)
+	c.JSON(http.StatusOK, randomUser)
 }
