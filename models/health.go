576307275834bef8bd21ac5a3186fea8d07869b6
Amine LOUHICHI <aminelch@pm.me>
2025-03-30 00:30:34 +0100

    refactor: enhance / route maintainability with structured approach

--- models/health.go
+++ models/health.go
@@ -18,3 +18,16 @@ type HealthResponse struct {
 	Version      string       `json:"version"`
 	Dependencies Dependencies `json:"dependencies"`
 }
+
+type Author struct {
+	Name    string `json:"name"`
+	Website string `json:"website"`
+}
+
+type HomePageResponse struct {
+	Api           string `json:"api"`
+	Version       string `json:"version"`
+	Status        string `json:"status"`
+	Documentation string `json:"documentation"`
+	Author        Author `json:"author"`
+}
